7dfdf38da00e1cebddbae24321c85b9c1eb9c806
KirillAshkere <45769560+KirillAshkere@users.noreply.github.com>
2023-05-04 04:06:06 +0300

    Add Enter_Delete_LocalStorage

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,23 @@
 const field = document.querySelector('.field');
-function createTask(value) {
+let tasksArray = [];
+
+if(localStorage.getItem('tasksArray')){
+    tasksArray = JSON.parse(localStorage.getItem('tasksArray'));
+}
+
+tasksArray.forEach(function(task){
     
+});
+
+document.querySelector('.add').addEventListener('click', addTask);
+document.querySelector('.field').addEventListener('keydown', function (e) {
+    if (e.keyCode === 13) {
+        addTask();
+    }
+})
+
+
+function createTask(value) {
     const task = document.createElement('div');
     task.textContent = value;
 
@@ -8,30 +25,91 @@ function createTask(value) {
     checkTask.setAttribute('type', 'checkbox');
     checkTask.classList.add('status');
     task.append(checkTask);
-    checkTask.addEventListener('click', completeTask)
+    checkTask.addEventListener('click', completeTask);
+
+    const btnDeleteTask = document.createElement('button');
+    btnDeleteTask.textContent = 'Удалить';
+    task.append(btnDeleteTask);
+    btnDeleteTask.addEventListener('click', funcDelTask);
+
+    task.classList.add('unsuccess');
+    
+
+// Создание массива тасок
+    const newObjTask = {
+        id: Date.now(),
+        text: value,
+        done: false
+    }
+    tasksArray.push(newObjTask);
+
+    saveToLocalStorage();
+
+    task.id = newObjTask.id;
     
     return task;
+
+};
+
+function funcDelTask(event) {
+    const targetDel = event.target;
+    const parentElementDel = targetDel.parentElement;
+    if(event.target){
+        parentElementDel.remove()
+    }
+
+    const id = Number(parentElementDel.id)
+
+    tasksArray = tasksArray.filter(function(task){
+        if (task.id === id) {
+            return false
+        } else{
+            return true
+        }
+    })
+    // Удаляем задачу через фильтрацию массива
+    // tasks = tasks.filter((task) => task.id !== id)
+
+    saveToLocalStorage();
+
 };
 
 
-function completeTask(event){
+function completeTask(event) {
     const target = event.target;
-    let parentElement = target.parentElement;
-    if(target.checked){
+    const parentElement = target.parentElement;
+    if (target.checked) {
         parentElement.classList.add('success');
-        alert('Хорош');
-    }else{
+        parentElement.classList.remove('unsuccess');
+    } else {
         parentElement.classList.remove('success');
+        parentElement.classList.add('unsuccess');
     }
+
+    const idTask = Number(parentElement.id);
+
+    const task = tasksArray.find(function(task){
+        if(task.id === idTask){
+            return true
+        }
+    });
+
+    task.done = !task.done
+
+    saveToLocalStorage()
 };
 
 function addTask() {
-    if (field.value != '') {
+    if (field.value !== '') {
         const newTask = createTask(field.value);
         const listAdd = document.querySelector('.list');
         listAdd.appendChild(newTask);
         field.value = '';
     };
 };
-document.querySelector('.add').addEventListener('click', addTask);
+
+
+function saveToLocalStorage(){
+    localStorage.setItem('tasksArray', JSON.stringify(tasksArray))
+}
 
